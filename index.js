853db0d281cdd65302327c33ad0a270ca8f6a48d
centino90 <anthonyjayansit2326282@gmail.com>
2021-01-09 03:30:50 -0800

    qwe

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-const tableElem = document.querySelectorAll('td, th');
+const createTip = (e) => {
+  e.stopPropagation();
+  const targ = e.target;
+  const table = e.target.parentNode.parentNode.parentNode
+  const ic = targ.querySelector('i')
+  const w = e.target.getBoundingClientRect().width;
+  if (w >= 176 && targ.textContent.length > 22 && !ic) {
+    const toolTip = document.createElement("div");
+    const tip = document.createElement("div");
+    let x = targ.offsetTop,
+      y = targ.offsetLeft;
+    toolTip.classList.add("tooltip");
+    tip.classList.add("tip");
+    toolTip.style.top = x + 35 + "px";
+    toolTip.style.left = y - 10 + "px";
+    tip.style.top = x + 25 + "px";
+    tip.style.left = y + "px";
+    toolTip.textContent = targ.textContent;
+    table.appendChild(toolTip);
+    table.appendChild(tip);
+  }
+};
+const removeTip = (e) => {
+  e.stopPropagation();
+  const table = e.target.parentNode.parentNode.parentNode;
+  const tip = table.querySelectorAll(".tooltip, .tip");
+  if (table.querySelector(".tooltip, .tip")) {
+    tip.forEach((elem) => {
+      console.log(1);
+      elem.remove();
+      return;
+    });
+  }
+};
+
+const tableElem = document.querySelectorAll("td, th");
 
 tableElem.forEach((elem) => {
-    elem.addEventListener('mouseover', e => {
-        e.stopPropagation()
-        const table = e.target.parentNode.parentNode.parentNode
-        const toolTip = document.createElement('div')
-        var x = e.target.offsetTop,
-            y = e.target.offsetLeft
-        // const att = document.createAttribute('full-detail')
-        // att.value = e.target.textContent
-        toolTip.classList.add('tooltip')
-        toolTip.style.top = (x + 35) + 'px';
-        toolTip.style.left = (y) + 'px';
-        toolTip.textContent = e.target.textContent
-        e.target.parentNode.parentNode.parentNode.appendChild(toolTip)
-
-    })
-})
+  elem.addEventListener("mouseover", createTip);
+});
 
 tableElem.forEach((elem) => {
-    elem.addEventListener('mouseleave', e => {
-        e.stopPropagation()
-        const table = e.target.parentNode.parentNode.parentNode
-        const tip = table.querySelector('div')
-        if (table.querySelector('div')) {
-            console.log(tip)
-            tip.remove()
-            return
-        }
-    })
-})
+  elem.addEventListener("mouseleave", removeTip);
+});
---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+      integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="./style.css" />
+  </head>
+  <body>
+    <div class="page-wrapper">
+      <table class="table">
+        <caption>
+          <form action="www.google.com" method="post" class="table-controls">
+            <div class="pagination-wrapper">
+              <button class="btn prev" type="button">Prev</button>
+              <button class="btn next" type="button">Next</button>
+            </div>
+            <div class="input-wrapper">
+              <input class="inp input" type="search" placeholder="input details..." />
+              <input class="inp submit" type="submit" value="search" />
+            </div>
+          </form>
+        </caption>
+        <caption>
+          <div class="title-wrapper">
+            <div class="left"><i class="fas fa-user"></i>users</div>
+            <div class="right limit">
+              <label for="limit">set limit</label>
+              <select name="" id="limit" class="inp">
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+              </select>
+            </div>
+          </div>
+        </caption>
+        <thead class="table__thead">
+          <tr>
+            <th>Fname</th>
+            <th>Lname</th>
+            <th>Job</th>
+            <th>Address</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody class="table__tbody">
+          <tr>
+            <td>Lorem</td>
+            <td>dolor</td>
+            <td>Lorem, ipsum dolor qwe w.</td>
+            <td>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed,
+              praesentium.
+            </td>
+            <td>
+              <i class="fas fa-ellipsis-v"></i>
+              <i class="far fa-trash-alt"></i>
+              <i class="far fa-edit"></i>
+            </td>
+          </tr>
+          <tr>
+            <td>Lorem</td>
+            <td>dolor</td>
+            <td>Lorem, ipsum dolor.</td>
+            <td>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed,
+              praesentium.
+            </td>
+            <td>
+              <i class="fas fa-ellipsis-v"></i>
+              <i class="far fa-trash-alt"></i>
+              <i class="far fa-edit"></i>
+            </td>
+          </tr>
+          <tr>
+            <td>Lorem</td>
+            <td>dolor</td>
+            <td>Lorem, ipsum dolor.</td>
+            <td>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed,
+              praesentium.
+            </td>
+            <td>
+              <i class="fas fa-ellipsis-v"></i>
+              <i class="far fa-trash-alt"></i>
+              <i class="far fa-edit"></i>
+            </td>
+          </tr>
+          <tr>
+            <td>Lorem</td>
+            <td>dolor</td>
+            <td>Lorem, ipsum dolor.</td>
+            <td>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed,
+              praesentium.
+            </td>
+            <td>
+              <i class="fas fa-ellipsis-v"></i>
+              <i class="far fa-trash-alt"></i>
+              <i class="far fa-edit"></i>
+            </td>
+          </tr>
+          <tr>
+            <td>Lorem</td>
+            <td>dolor</td>
+            <td>Lorem, ipsum dolor.</td>
+            <td>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed,
+              praesentium.
+            </td>
+            <td>
+              <i class="fas fa-ellipsis-v"></i>
+              <i class="far fa-trash-alt"></i>
+              <i class="far fa-edit"></i>
+            </td>
+          </tr>
+          <tr>
+            <td>Lorem</td>
+            <td>dolor</td>
+            <td>Lorem, ipsum dolor.</td>
+            <td>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed,
+              praesentium.
+            </td>
+            <td>
+              <i class="fas fa-ellipsis-v"></i>
+              <i class="far fa-trash-alt"></i>
+              <i class="far fa-edit"></i>
+            </td>
+          </tr>
+          <tr>
+            <td>Lorem</td>
+            <td>dolor</td>
+            <td>Lorem, ipsum dolor.</td>
+            <td>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed,
+              praesentium.
+            </td>
+            <td>
+              <i class="fas fa-ellipsis-v"></i>
+              <i class="far fa-trash-alt"></i>
+              <i class="far fa-edit"></i>
+            </td>
+          </tr>
+          <tr>
+            <td>Lorem</td>
+            <td>dolor</td>
+            <td>Lorem, ipsum dolor.</td>
+            <td>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed,
+              praesentium.
+            </td>
+            <td>
+              <i class="fas fa-ellipsis-v"></i>
+              <i class="far fa-trash-alt"></i>
+              <i class="far fa-edit"></i>
+            </td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>Reminder: further details can be accessed within action</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+    <script src="./index.js"></script>
+  </body>
+</html>
